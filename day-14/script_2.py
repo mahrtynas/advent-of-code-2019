0ec6e21d3f0dc015e8442579cb1a9f3752b0f44c
smartynas <mseskaitis@gmail.com>
2019-12-16 23:50:24 +0100

    day 13 and 14

---
+++ day-14/script_2.py
@@ -0,0 +1,83 @@
+import math
+
+def get_reactions(path):
+	reactions = dict()
+	with open(path, 'r') as f:
+		file_lines = f.readlines()
+	for line in file_lines:
+		line_parts = line.split("=>")
+		target = line_parts[1].strip("\n| ")
+		inputs = [x.strip(" ") for x in line_parts[0].split(",")]
+		reactions[target] = inputs
+	return reactions
+
+def parse_value(value):
+	[val, name] = value.split(" ")
+	val = int(val)
+	return name, val
+
+class Element():
+	def __init__(self, name, min_value):
+		self.name = name
+		self.min_value = int(min_value)
+		self.dependent_elements = []
+		self.dependent_amounts = []
+		self.available = 0
+		self.demand = 0
+	
+	def __str__(self):
+		return (f'Info about element {self.name}:\n'
+			f'Dependents: {[x.name for x in self.dependent_elements]}\n'
+			f'Dependent amounts: {self.dependent_amounts}\n'
+			f'Demand: {self.demand}\n'
+			f'Available: {self.available}')
+
+	def assign_dependents(self, de, value):
+		self.dependent_elements.append(de)
+		self.dependent_amounts.append(value)
+
+	def produce(self, requested_amount):
+		self.demand += requested_amount
+		if self.available >= requested_amount:
+			self.available -= requested_amount
+		else:
+			additional = requested_amount - self.available
+			production_quantity = math.ceil(additional / self.min_value)
+			if self.name != "ORE":
+				for i, elem in enumerate(self.dependent_elements):
+					elem.produce(production_quantity * self.dependent_amounts[i])
+			self.available += production_quantity * self.min_value
+			self.available -= requested_amount
+
+def main():
+	reactions = get_reactions("input.txt")
+	elements = dict(ORE = Element('ORE', 1))
+
+	# initiate all elements
+	print(reactions)
+	for target in reactions:
+		name, val = parse_value(target)
+		elements[name] = Element(name, val)
+	for react in reactions.items():
+		el_name, val = parse_value(react[0])
+		for i in react[1]:
+			name, val = parse_value(i)
+			elements[el_name].assign_dependents(elements[name], val)
+	
+	max_ore = 1000000000000
+	ore = 0
+	i = 0
+	initial_guess = 3100000
+	elements['FUEL'].produce(initial_guess)
+	while ore < max_ore:
+		if (i % 10000 == 0):
+			print(f'{ore:,}')
+		elements['FUEL'].produce(1)
+		ore = elements['ORE'].demand
+		i += 1
+	print(f'Max fuel = {initial_guess + i - 1}')
+
+	
+
+if __name__ == "__main__":
+	main()
